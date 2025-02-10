440e0aaf22da91f94258895b2a68d74ec1c3ab04
Jeong Ji Won <wldnjs9705@penta.co.kr>
2025-02-10 16:23:47 +0900

    study complete section6

---
+++ onebite-next/section03/src/app/(with-searchbar)/page.tsx
@@ -0,0 +1,69 @@
+import BookItem from '@/components/book-item';
+import style from './page.module.css';
+import books from '@/mock/books.json';
+import { BookData } from '@/types';
+import { Suspense } from 'react';
+import BookItemSkeleton from '@/components/skeleton/book-item-skeleton';
+import BookListSkeleton from '@/components/skeleton/book-list-skeleton';
+export const dynamic = 'force-dynamic';
+/* 특정 페이지의 유형을 강제로 static, Dynamic 페이지로 설정
+   1. auto
+   2. force-dynamic
+   3. force-static
+   4. error
+ */
+
+async function AllBooks() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
+    { cache: 'force-cache' }
+  );
+  if (!response.ok) {
+    return <div>오류가 발생했습니다..</div>;
+  }
+  const allBooks: BookData[] = await response.json();
+
+  return (
+    <div>
+      {allBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
+async function RecoBooks() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
+    { next: { revalidate: 3 } }
+  );
+  if (!response.ok) {
+    return <div>오류가 발생했습니다..</div>;
+  }
+  const recoBooks: BookData[] = await response.json();
+  return (
+    <div>
+      {recoBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
+
+export default function Home() {
+  return (
+    <div className={style.container}>
+      <section>
+        <h3>지금 추천하는 도서</h3>
+        <Suspense fallback={<BookListSkeleton count={3} />}>
+          <RecoBooks />
+        </Suspense>
+      </section>
+      <section>
+        <h3>등록된 모든 도서</h3>
+        <Suspense fallback={<BookListSkeleton count={10} />}>
+          <AllBooks />
+        </Suspense>
+      </section>
+    </div>
+  );
+}
